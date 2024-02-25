9846ed9775fa823be768cbe7b5db1b8c233c1e41
Nikunj Mehra <nikunjmehra935@gmail.com>
2024-02-25 11:49:51 +0530

    clerk fix

--- components/providers/convex-provider.tsx
+++ components/providers/convex-provider.tsx
@@ -1,17 +1,16 @@
 "use client";
 
-import { PropsWithChildren } from "react";
+import { ReactNode } from "react";
 import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ClerkProvider, useAuth } from "@clerk/clerk-react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
-export const ConvexClientProvider = ({ children }: PropsWithChildren) => {
+export const ConvexClientProvider = ({ children }: { children: ReactNode }) => {
   return (
     <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-      frontendApi={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
     >
       <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
         {children}
